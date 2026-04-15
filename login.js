92cbc960549e21a1b7f2bcd6e7a14abb73885c3e
bharat rokaya <bharat12rokaya123@gmail.com>
2026-04-15 23:45:46 +0545

    Login feature added

---
+++ login.js
@@ -0,0 +1,37 @@
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const emailError = document.getElementById("email-error");
+const passwordError = document.getElementById("password-error");
+const loginButton = document.getElementById("login");
+
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  emailError.textContent = "";
+  passwordError.textContent = "";
+
+  loginButton.textContent = "Processing...";
+
+  setTimeout(() => {
+    let isValidate = true;
+
+    if (!email) {
+      emailError.textContent = "Email is required";
+      isValidate = false;
+    } else if (!password) {
+      passwordError.textContent = "Password is required";
+      isValidate = false;
+    } else if (password.length < 6) {
+      passwordError.textContent = "Password must be at least 6 characters";
+      isValidate = false;
+    }
+
+    loginButton.textContent = "Login";
+
+    if (isValidate) {
+      alert("Login successful!");
+      window.location.href = "index.html";
+    }
+  }, 1000);
+});
